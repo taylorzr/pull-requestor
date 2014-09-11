5f48a0fd33ca1c17661a121fef4e2222d586a8d6
Zach Taylor <taylorzr@gmail.com>
2014-09-10 22:53:58 -0500

    Completes basic functionality

--- github.rb
+++ github.rb
@@ -1,8 +1,49 @@
 require 'octokit'
 
-print "Password: "
-password = gets.chomp
+## Collect git info
 
-github = Octokit::Client.new(login: "taylorzr", password: password)
+  # if not configured?
+  github_username = `git config github.user`.chomp
 
-puts github.user.inspect
+  # if not in git project?
+  git_remote = `git remote -v | grep "\(push\)" | egrep -o "/[^/]+/[^/]+['.git']" | egrep -o ".+[^'.git']"`.chomp
+  _, remote_username, remote_repo = git_remote.split("/")
+  base_branch = "master"
+  current_branch = `git branch | grep "\*" | grep -o "[^'* '].*"`.chomp
+  puts "  Making merge request for #{current_branch} -> #{base_branch} @ #{remote_username}/#{remote_repo}"
+
+
+## Get user input
+
+  print "  Password: "
+  password = gets.chomp
+
+  github = Octokit::Client.new(login: "taylorzr", password: password)
+
+  message = ""
+  while message == ""
+    print "  Message for pull-request: "
+    message = gets.chomp
+    puts "  Error: Message can not be blank" if message == ""
+  end
+
+
+## Make pull request
+  begin
+    pull_request = github.create_pull_request(
+      "#{github_username}/#{remote_repo}", 
+      "master", 
+      "#{current_branch}", 
+      "#{message}", 
+      ""
+    )
+    puts "  Success: Pull request made!"
+  rescue StandardError => error
+    if error.class == Octokit::UnprocessableEntity
+      puts "  Error: A pull-request from this branch already exists"
+    elsif error.class == Octokit::Unauthorized
+      puts "  Error: Invalid password"
+    else
+      puts "  Error: #{error}"
+    end
+  end
